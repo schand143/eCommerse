f4fc4dda1bbafb49ef1da90f30a0c7bd4c426b0f
schand143 <shaikhchand724@gmail.com>
2020-05-08 14:45:53 +0530

    Updates

---
+++ precache-manifest.b8d69e29d46e96cd6dc586557540ee53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d84b92b4f8a331aa12c9ae8d3765c77",
+    "url": "/eCommerse/index.html"
+  },
+  {
+    "revision": "97db0939d228f764f62e",
+    "url": "/eCommerse/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2286577794c1f91178f3",
+    "url": "/eCommerse/static/js/2.2c1c0eee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eCommerse/static/js/2.2c1c0eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97db0939d228f764f62e",
+    "url": "/eCommerse/static/js/main.960a80e7.chunk.js"
+  },
+  {
+    "revision": "e55f78d97ea0a6f7ba7c",
+    "url": "/eCommerse/static/js/runtime-main.9c67effb.js"
+  }
+]);
